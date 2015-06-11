f0811fa7ee2423aa40165ab6aa0de33f705eccb2
JoseArceDev <jose.arce.dev@gmail.com>
2015-06-11 10:12:48 -0500

    commit inicial

---
+++ AGTMoney/AGTMoney/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AGTMoney
+//
+//  Created by koombea on 6/11/15.
+//  Copyright (c) 2015 koombea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AGTMoney/AGTMoney/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AGTMoney
+//
+//  Created by koombea on 6/11/15.
+//  Copyright (c) 2015 koombea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
